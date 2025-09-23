c58185760b2d087f8155d99b6ffe35c81e145d52
BG-Home-Official <bghomeofficial@gmail.com>
2025-09-23 08:59:19 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 import os
 
-TOKEN = os.getenv("DISCORD_TOKEN")  # Or replace with your bot token string
+TOKEN = "MTQwNjU4NDEyNTAxOTkxNDQ1NA.GVeo2l.4bTzM843y6DqY7SB7gRCaijp6_8-j3ZkPqN7hY"  # Or replace with your bot token string
 
 intents = discord.Intents.default()
 intents.message_content = True  # Needed to read messages
